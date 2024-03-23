d6f31de23a36e003fc8d31bb56b80711314eee8e
fenil3007 <151509519+fenil3007@users.noreply.github.com>
2024-03-23 15:51:11 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld{
+     static  public void  main (String[] args){
+    System.out.println("Hello , World!");
+}
+
+    static public void main (int[] args){
+        System.out.println("HELLO , WORLD!");
+    }
+    }
+/*public class Test{
+    static  public void  main ( String[] args){
+   System.out.println("Hello , World!");
+   }
+}*/
